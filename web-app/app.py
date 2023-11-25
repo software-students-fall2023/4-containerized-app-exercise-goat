df2cb01482d4e61d478714ac8394cb78777f7040
Jason-SL-Zhang <sz3042@nyu.edu>
2023-11-25 15:09:40 -0500

    sample web app

--- web-app/app.py
+++ web-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import random
 
 app = Flask(__name__)
@@ -6,24 +6,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Generate a random addition or subtraction problem
-    operator = random.choice(['+', '-'])
     num1 = random.randint(1, 9)
     num2 = random.randint(1, 9)
-    problem = f"{num1} {operator} {num2} = ?"
+    operation = random.choice(['+', '-'])
+    correct_answer = str(eval(f"{num1} {operation} {num2}"))
 
-    # Generate 3 incorrect options and 1 correct option
-    options = [chr(ord('A') + i) for i in range(4)]
-    correct_option = random.choice(options)
-    options.remove(correct_option)
-    random.shuffle(options)
-    answers = {
-        'A': num1 + num2 if correct_option == 'A' else random.randint(1, 18),
-        'B': num1 + num2 if correct_option == 'B' else random.randint(1, 18),
-        'C': num1 + num2 if correct_option == 'C' else random.randint(1, 18),
-        'D': num1 + num2 if correct_option == 'D' else random.randint(1, 18),
-    }
+    # Generate three random wrong answers
+    wrong_answers = [str(random.randint(1, 18)) for _ in range(3)]
 
-    return render_template('index.html', problem=problem, options=options, answers=answers)
+    # Combine correct and wrong answers and shuffle them
+    answers = [correct_answer] + wrong_answers
+    random.shuffle(answers)
+
+    # Convert answers to dictionary format for easier JSON serialization
+    choices = {chr(65 + i): answers[i] for i in range(4)}
+
+    return render_template('index.html', num1=num1, num2=num2, operation=operation, choices=choices)
 
 if __name__ == '__main__':
     app.run(debug=True)
