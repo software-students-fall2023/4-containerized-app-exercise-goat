be265bc706d1b2b9c976f35ac7e00381b0299a31
SpencerWPak <spencerpak9999@gmail.com>
2023-12-03 17:45:01 -0500

    pylinted

--- web-app/app.py
+++ web-app/app.py
@@ -1,74 +1,75 @@
-import random
+"""
+This module contains a Flask web application for a math question game with audio input handling.
+"""
+
 import os
-from flask import Flask, jsonify, request, render_template, redirect, url_for, make_response
-from pydub import AudioSegment
-import db 
+import random
+from flask import Flask, jsonify, request, render_template, make_response
 import requests
+from pydub import AudioSegment
+import db
 
 app = Flask(__name__, template_folder='templates')
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-current_question = None
-attempts = 0
-correct_answer = "1"
+CURRENT_QUESTION = None
+ATTEMPTS = 0
+CORRECT_ANSWER = "1"
 
 def generate_question():
-    global current_question
-    # Generate a random addition or subtraction problem
+    """Generate a random addition or subtraction problem."""
+    global CURRENT_QUESTION, CORRECT_ANSWER
     num1 = random.randint(1, 9)
     num2 = random.randint(1, num1)
     operation = random.choice(['+', '-'])
-    global correct_answer
-    correct_answer = str(eval(f"{num1} {operation} {num2}"))
+    CORRECT_ANSWER = str(num1 + num2 if operation == '+' else num1 - num2)
 
-    current_question = {
+    CURRENT_QUESTION = {
         'num1': num1,
         'num2': num2,
         'operation': operation,
-        'correct_answer': correct_answer,
+        'correct_answer': CORRECT_ANSWER,
     }
+
 def get_transcript():
+    """Fetch the most recent transcript from a specified URL."""
     target_url = 'http://mlc:3000/transcript'
     response = requests.get(target_url)
-    if (response):
-        transcript = db.get_most_recent_transcript()
-        return transcript
-    return "there is an error fetching a transcript"
+    if response:
+        return db.get_most_recent_transcript()
+    return "Error fetching transcript"
+
 @app.route('/')
 def index():
+    """Render the main page with a new question."""
     generate_question()
-    return render_template('LetterMath.html', current_question=current_question)
+    return render_template('LetterMath.html', current_question=CURRENT_QUESTION)
 
 @app.route('/upload-audio', methods=['POST'])
 def upload_audio():
+    """Handle audio file upload and validate the answer."""
     if request.method == "POST":
-        f= request.files['audio_data']
-        with open('uploads/audio.wav', 'wb') as audio:
-            f.save(audio)
-        print('file uploaded successfully')
-        transcript= get_transcript() or "no transcript"
+        audio_file = request.files['audio_data']
+        audio_file.save(os.path.join(UPLOAD_FOLDER, 'audio.wav'))
+        print('File uploaded successfully')
+        transcript = get_transcript() or "No transcript"
         print(transcript)
-        print(correct_answer)
-        transcript = str(transcript.strip())
-        if transcript == correct_answer:
-            isRight = "True"
-        else:
-            isRight = "False"
-        response = make_response(jsonify({'transcript': transcript, 'isRight': isRight, 'correct_answer': correct_answer}))
+        print(CORRECT_ANSWER)
+        transcript = transcript.strip()
+        is_right = "True" if transcript == CORRECT_ANSWER else "False"
+        response = make_response(jsonify({'transcript': transcript, 'isRight': is_right, 'correct_answer': CORRECT_ANSWER}))
         return response
-    else:
-        return render_template("LetterMath.html",current_question=current_question)
-    
+    return render_template("LetterMath.html", current_question=CURRENT_QUESTION)
+
 def check_file():
-    audio = AudioSegment.from_file("uploads/blob")
+    """Check the properties of the uploaded audio file."""
+    audio = AudioSegment.from_file(os.path.join(UPLOAD_FOLDER, 'blob'))
     print("Channels:", audio.channels)
     print("Sample Width (bytes):", audio.sample_width)
     print("Sample Rate (Hz):", audio.frame_rate)
 
 if __name__ == '__main__':
-    #check_file()
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    #convert_blob_file_to_wav()
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
     app.run(host='0.0.0.0', port=4000)
