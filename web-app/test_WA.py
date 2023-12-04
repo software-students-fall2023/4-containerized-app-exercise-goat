0f8ef1566c185f7664eafeca80ba1e0d9af83a54
AlvisYan2025 <hy2275@nyu.edu>
2023-12-04 01:45:54 -0500

    reformat

--- web-app/test_WA.py
+++ web-app/test_WA.py
@@ -4,7 +4,7 @@
 # pylint: disable=no-member
 # pylint: disable=no-name-in-module
 
-'''import modules'''
+"""import modules"""
 import pytest
 from app import create_app
 from db import get_most_recent_transcript
@@ -12,7 +12,7 @@ from db import get_most_recent_transcript
 
 @pytest.fixture
 def app():
-    '''create app'''
+    """create app"""
     app_instance = create_app()
     app_instance.config["TESTING"] = True
     return app_instance
@@ -20,61 +20,63 @@ def app():
 
 @pytest.fixture
 def client(app):
-    '''create client'''
+    """create client"""
     return app.test_client()
 
 
 def test_index(client):
-    '''test index page'''
+    """test index page"""
     response = client.get("/")
     assert response.status_code == 200
 
 
 def test_upload_audio(client):
-    '''test upload audio function'''
+    """test upload audio function"""
     response = client.get("/upload-audio")
     assert response
 
 
 def test_connection(client):
-    '''test connection to app'''
+    """test connection to app"""
     response = client.get("/test")
     assert response.status_code == 200
 
 
 def test_index_route(client):
-    '''test index route'''
+    """test index route"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"current_question" in response.data
 
 
 def test_upload_audio_post(client, monkeypatch):
-    '''test upload audio route'''
+    """test upload audio route"""
     monkeypatch.setattr("app.get_transcript", lambda: "transcript")
     response = client.post(
         "/upload-audio", data={"audio_data": (b"fake_audio_data", "audio.wav")}
     )
     assert response
 
+
 def test_cheat(client):
-    '''test cheat route'''
+    """test cheat route"""
     response = client.get("/cheat")
     assert response.status_code == 200
 
 
 def test_show_answer(client):
-    '''test show answer route'''
+    """test show answer route"""
     response = client.get("/gimmeanswer")
     assert response.status_code == 200
 
+
 def test_instruction(client):
-    '''test instruction route'''
+    """test instruction route"""
     response = client.get("/instruction")
     assert response.status_code == 200
 
 
 def test_db_error():
-    '''test behavior if db error'''
+    """test behavior if db error"""
     message = get_most_recent_transcript()
     assert message is None
