02920b66b4aa51e3126fb3592fb0899e37990b2e
Henry Greene <hxg204@nyu.edu>
2023-12-04 05:00:38 +0000

    linting

--- web-app/test_WA.py
+++ web-app/test_WA.py
@@ -1,12 +1,20 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=redefined-outer-name
+# pylint: disable=missing-function-docstring
+# pylint: disable=unused-argument
+# pylint: disable=trailing-whitespace
+# pylint: disable=invalid-name
+# pylint: disable=singleton-comparison
+
 import pytest
 from app import create_app
-import json
+from db import get_most_recent_transcript
 
 @pytest.fixture
 def app():
-    app = create_app()
-    app.config['TESTING'] = True
-    return app
+    app_instance = create_app()
+    app_instance.config['TESTING'] = True
+    return app_instance
 
 @pytest.fixture
 def client(app):
@@ -15,15 +23,14 @@ def client(app):
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
-    
+
 def test_upload_audio(client):
     response = client.get('/upload-audio')
     assert response
 
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
+def test_connection(client):
+    response = client.get('/test')
+    assert response.status_code == 200
 
 def test_index_route(client):
     response = client.get('/')
@@ -35,10 +42,19 @@ def test_upload_audio_post(client, monkeypatch):
     response = client.post('/upload-audio', data={'audio_data': (b'fake_audio_data', 'audio.wav')})
     assert response
 
-def test_upload_audio_post_error(client, monkeypatch):
-    monkeypatch.setattr("app.get_transcript", lambda: None)
-    response = client.post('/upload-audio', data={'audio_data': (b'fake_audio_data', 'audio.wav')})
-    assert response.status_code == 500
-    data = json.loads(response.get_data(as_text=True))
-    assert 'error' in data
+# pylint: disable=trailing-whitespace
+def test_cheat(client):
+    response = client.get('/cheat')
+    assert response.status_code == 200
 
+def test_show_answer(client):
+    response = client.get('/gimmeanswer')
+    assert response.status_code == 200
+
+def test_instruction(client):
+    response = client.get('/instruction')
+    assert response.status_code == 200
+
+def test_db_error():
+    message = get_most_recent_transcript()
+    assert message is None
