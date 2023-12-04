f40b73e1e43f11c9eb875e5524cd0cb14f4b2084
Henry Greene <hxg204@nyu.edu>
2023-12-04 05:36:51 +0000

    reformat

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -1,23 +1,27 @@
-'''make a flask based app'''
+"""make a flask based app"""
 from flask import Flask, jsonify, request, make_response
 import db
 import speechToText
 
 app = Flask(__name__)
 
-@app.route('/transcript', methods=['GET'])
+
+@app.route("/transcript", methods=["GET"])
 def recognize_and_save(my_id=1):
-    '''fetch the transcript from the database'''
-    my_id = request.args.get('Id')
+    """fetch the transcript from the database"""
+    my_id = request.args.get("Id")
     transcript = speechToText.get_transcript()
-    db.save_transcript(transcript,my_id)
-    response = make_response(jsonify({'message': 'transcript saved!'}))
+    db.save_transcript(transcript, my_id)
+    response = make_response(jsonify({"message": "transcript saved!"}))
     return response
 
-@app.route('/',methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def test():
-    '''for testing purpose, this will display a message when a connection is successful'''
-    response = make_response(jsonify({'message': 'hi'}))
+    """for testing purpose, this will display a message when a connection is successful"""
+    response = make_response(jsonify({"message": "hi"}))
     return response
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
--- web-app/test_WA.py
+++ web-app/test_WA.py
@@ -10,50 +10,63 @@ import pytest
 from app import create_app
 from db import get_most_recent_transcript
 
+
 @pytest.fixture
 def app():
     app_instance = create_app()
-    app_instance.config['TESTING'] = True
+    app_instance.config["TESTING"] = True
     return app_instance
+
 
 @pytest.fixture
 def client(app):
     return app.test_client()
 
+
 def test_index(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
+
 
 def test_upload_audio(client):
-    response = client.get('/upload-audio')
+    response = client.get("/upload-audio")
     assert response
 
+
 def test_connection(client):
-    response = client.get('/test')
+    response = client.get("/test")
     assert response.status_code == 200
 
+
 def test_index_route(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert b'current_question' in response.data
+    assert b"current_question" in response.data
+
 
 def test_upload_audio_post(client, monkeypatch):
     monkeypatch.setattr("app.get_transcript", lambda: "transcript")
-    response = client.post('/upload-audio', data={'audio_data': (b'fake_audio_data', 'audio.wav')})
+    response = client.post(
+        "/upload-audio", data={"audio_data": (b"fake_audio_data", "audio.wav")}
+    )
     assert response
+
 
 # pylint: disable=trailing-whitespace
 def test_cheat(client):
-    response = client.get('/cheat')
+    response = client.get("/cheat")
     assert response.status_code == 200
+
 
 def test_show_answer(client):
-    response = client.get('/gimmeanswer')
+    response = client.get("/gimmeanswer")
     assert response.status_code == 200
 
+
 def test_instruction(client):
-    response = client.get('/instruction')
+    response = client.get("/instruction")
     assert response.status_code == 200
+
 
 def test_db_error():
     message = get_most_recent_transcript()
