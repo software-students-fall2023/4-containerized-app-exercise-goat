b19c1d6dd1cfe620755d275abbc92e7f63faaa22
Henry Greene <hxg204@nyu.edu>
2023-12-04 05:41:36 +0000

    format

--- machine-learning-client/speechToText.py
+++ machine-learning-client/speechToText.py
@@ -1,28 +1,31 @@
-'''use Google Cloud Speech-To-Text API'''
+"""use Google Cloud Speech-To-Text API"""
 # pylint: disable=invalid-name
 import io
 from google.cloud import speech
 from pydub import AudioSegment
 
+
 def convert_stereo_to_mono(input_wav, output_wav):
-    '''convert a double channel wav to a single channel wav'''
+    """convert a double channel wav to a single channel wav"""
     audio = AudioSegment.from_wav(input_wav)
 
     audio = audio.set_channels(1)
 
     audio.export(output_wav, format="wav")
+
+
 def run_quickstart(local_file_path) -> speech.RecognizeResponse:
-    '''get transcript from google cloud speech-to-text'''
+    """get transcript from google cloud speech-to-text"""
     # Instantiates a client
     client = speech.SpeechClient()
 
     # The name of the audio file to transcribe
-    #gcs_uri = "machine-learning-client/77837^goodmrng.mp3"
-    #gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
-    #audio = speech.RecognitionAudio(uri=gcs_uri)
+    # gcs_uri = "machine-learning-client/77837^goodmrng.mp3"
+    # gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+    # audio = speech.RecognitionAudio(uri=gcs_uri)
     with io.open(local_file_path, "rb") as f:
         content = f.read()
-    #print(content)
+    # print(content)
     audio = {"content": content}
 
     config = speech.RecognitionConfig(
@@ -37,9 +40,11 @@ def run_quickstart(local_file_path) -> speech.RecognizeResponse:
     for result in response.results:
         output += f"{result.alternatives[0].transcript}"
     return output
+
+
 def get_transcript():
-    '''main function'''
-    input_wav = 'uploads/audio.wav'
-    output_wav = 'curr.wav'
-    convert_stereo_to_mono(input_wav,output_wav)
+    """main function"""
+    input_wav = "uploads/audio.wav"
+    output_wav = "curr.wav"
+    convert_stereo_to_mono(input_wav, output_wav)
     return run_quickstart(output_wav)
--- machine-learning-client/test_ML.py
+++ machine-learning-client/test_ML.py
@@ -1,6 +1,6 @@
-#pylint: disable=missing-module-docstring
-#pylint: disable=invalid-name
-#pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=invalid-name
+# pylint: disable=missing-function-docstring
 
 import sys
 from unittest.mock import patch, Mock
@@ -8,25 +8,27 @@ import io
 from speechToText import run_quickstart, convert_stereo_to_mono
 
 
-sys.modules['voicerecorder'] = Mock()
-sys.modules['db'] = Mock()
-sys.modules['speechToText'] = Mock()
+sys.modules["voicerecorder"] = Mock()
+sys.modules["db"] = Mock()
+sys.modules["speechToText"] = Mock()
+
 
 def test_convert_stereo_to_mono():
-    input_wav = 'test_input.wav'
-    output_wav = 'test_output.wav'
-    with patch('pydub.AudioSegment.from_wav') as mock_from_wav:
+    input_wav = "test_input.wav"
+    output_wav = "test_output.wav"
+    with patch("pydub.AudioSegment.from_wav") as mock_from_wav:
         mock_audio_segment = mock_from_wav.return_value
         mock_audio_segment.set_channels.return_value = mock_audio_segment
         convert_stereo_to_mono(input_wav, output_wav)
         mock_audio_segment.set_channels.assert_called_once_with(1)
         mock_audio_segment.export.assert_called_once_with(output_wav, format="wav")
 
+
 def test_run_quickstart():
-    local_file_path = 'test.wav'
-    with patch('google.cloud.speech.SpeechClient') as mock_speech_client:
+    local_file_path = "test.wav"
+    with patch("google.cloud.speech.SpeechClient") as mock_speech_client:
         mock_client_instance = Mock()
         mock_speech_client.return_value = mock_client_instance
-        with patch.object(mock_client_instance, 'recognize'):
-            with patch('builtins.open', return_value=io.StringIO()):
+        with patch.object(mock_client_instance, "recognize"):
+            with patch("builtins.open", return_value=io.StringIO()):
                 run_quickstart(local_file_path)
